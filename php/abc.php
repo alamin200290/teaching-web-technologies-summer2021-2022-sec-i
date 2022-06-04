58fde8e670e25d343c629a019bc9989d7ee40abe
alamin200290c <al.amin@crowdbotics.com>
2022-06-04 12:00:11 +0600

    intro php

---
+++ php/abc.php
@@ -0,0 +1,29 @@
+<?php 
+
+	$name = "alamin";
+	$student = array('1',4,4.5);
+	$student = ['alamin', 'alamin@aiub.edu', 4.6];
+	$student = ['name'=>'alamin', 'email'=>'alamin@aiub.edu', 'cgpa'=>4.6];
+	//$student['name'];
+
+/*	echo $name."<br>";
+	print($student['cgpa']);*/
+
+/*	for ($i=0; $i < 10 ; $i++) { 
+		echo $name."<br>";
+	}
+
+	if (condition) {
+		// code...
+	}*/
+
+	function sum(){
+		$a=10;
+		$b=20;
+		return $a+$b;
+	}
+	echo sum();
+?>
+
+
+
